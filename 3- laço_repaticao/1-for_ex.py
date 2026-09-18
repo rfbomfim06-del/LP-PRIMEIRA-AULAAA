cbd633963f4804a3d3a8d686105054a13e3149fe
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-09-18 16:46:19 -0300

    adicionado laço de repetiçao

---
+++ 3- laço_repaticao/1-for_ex.py
@@ -0,0 +1,5 @@
+import os
+os.system('cls')
+
+for i in range(1,6):
+    print(f'o numero é:{i}')
---
+++ 3- laço_repaticao/2-tabuada.py
@@ -0,0 +1,8 @@
+import os
+os.system('cls')
+
+print('= TABUADA =')
+numero = int(input('digite um numero: '))
+
+for i in range (1,11):
+    print(f'{numero} / {i} = {numero / i} ')
